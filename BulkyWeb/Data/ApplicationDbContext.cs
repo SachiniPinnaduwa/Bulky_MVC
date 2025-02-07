5b96f254d8031c72f2f74f95c30318008772d343
SachiniPinnaduwa <sachinipinnaduwa2@gmail.com>
2025-02-07 09:15:22 +0530

    Added CategoryController, Category views, and seeded Category table

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -11,5 +11,14 @@ namespace BulkyWeb.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                    new Category { Id = 1, Name = "Action", DisplayedOrder = 1},
+                    new Category { Id = 2, Name = "SciFi", DisplayedOrder = 2 },
+                    new Category { Id = 3, Name = "History", DisplayedOrder = 3 }
+                );
+        }
     }
 }
